59c8170ef766b3c0da9b5a5c9136e58bb0c5084e
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-07 21:45:38 +0200

    add mirror color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   purge: [],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
-
-      smart:"SF Pro Display,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif"
+      smart:
+        "SF Pro Display,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif",
+    },
+    colors:{
+      mirror:"#2f3236"
     }
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
