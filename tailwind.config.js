d70ce1f3da104ad25dca28accee25ae475f025b5
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-07 21:23:47 +0200

    add new font combination

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+
+      smart:"SF Pro Display,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif"
+    }
   },
   variants: {
     extend: {},
