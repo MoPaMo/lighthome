72e85b76860f15abcc9e7033bf187618dea9d905
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-07 22:00:47 +0200

    use mirror colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         "SF Pro Display,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif",
     },
     colors: {
-      mirror: "#2f3236",
+      mirror: { 500: "#2f3236", 400: "#474c52" },
       transparent: "transparent",
       current: "currentColor",
       black: colors.black,
