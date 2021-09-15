bbc9687dc838c0eb5409293fadebeca807cc75c6
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-15 23:07:37 +0200

    export extend to function to avoid asynchronity

--- script.js
+++ script.js
@@ -7,26 +7,27 @@ document
       .scrollIntoView({ behavior: "smooth" });
     return false;
   });
+  const setListener=(t)=>{
+    document
+      .getElementById(t + "-extend")
+      .addEventListener("click", (e) => {
+        context = document.getElementById(t + "-context");
+        usual = document.getElementById(t + "-usual");
+        console.log(usual);
+        usual.classList.toggle("opacity-0");
+        usual.classList.toggle("opacity-100");
+        usual.classList.toggle("top-0");
+        usual.classList.toggle("inset-y-32");
+
+        context.classList.toggle("opacity-100");
+        context.classList.toggle("opacity-0");
+        context.classList.toggle("top-56");
+        context.classList.toggle("top-0");
+      });
+  }
 let context, usual;
 const toggles = ["mirror", "f"];
 for (t in toggles) {
   t = toggles[t];
-  console.log(t);
-
-  document
-    .getElementById(t + "-extend")
-    .addEventListener("click", (e) => {
-      console.log(e);
-      context = document.getElementById(t + "-context");
-      usual = document.getElementById(t + "-usual");
-      usual.classList.toggle("opacity-0");
-      usual.classList.toggle("opacity-100");
-      usual.classList.toggle("top-0");
-      usual.classList.toggle("inset-y-32");
-
-      context.classList.toggle("opacity-100");
-      context.classList.toggle("opacity-0");
-      context.classList.toggle("top-56");
-      context.classList.toggle("top-0");
-    });
+setListener(t)
 }
