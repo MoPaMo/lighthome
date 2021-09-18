cbfcff0ce38641791238785d7a6ca7ef48e0bde2
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-18 10:42:13 +0200

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [],
+  purge: { enabled: true, content: ["*.html"] },
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
