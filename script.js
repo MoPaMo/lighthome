6942157fbc442a3b96d30247ae7ead7d8f86cd39
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-23 17:37:24 +0200

    automatically generate smooth scrolling listeners

--- script.js
+++ script.js
@@ -1,57 +1,39 @@
 window.__forceSmoothScrollPolyfill__ = true;
-document
-  .getElementById("header-scrolldown")
-  .addEventListener("click", () => {
-    document
-      .querySelector("#cardparent")
-      .scrollIntoView({ behavior: "smooth" });
-    return false;
-  });
-  document
-  .querySelector(".icon-1")
-  .addEventListener("click", () => {
-    document
-      .querySelector("#mirror-usual")
-      .scrollIntoView({ behavior: "smooth" });
-    return false;
-  });  
-  document
-  .querySelector(".icon-2")
-  .addEventListener("click", () => {
-    document
-      .querySelector("#f-usual")
-      .scrollIntoView({ behavior: "smooth" });
-    return false;
-  });
-  document
-  .querySelector(".icon-3")
-  .addEventListener("click", () => {
-    document
-      .querySelector("#sl-usual")
-      .scrollIntoView({ behavior: "smooth" });
-    return false;
-  });
-  const setListener=(t)=>{
-    document
-      .getElementById(t + "-extend")
-      .addEventListener("click", (e) => {
-        context = document.getElementById(t + "-context");
-        usual = document.getElementById(t + "-usual");
-        console.log(usual);
-        usual.classList.toggle("opacity-0");
-        usual.classList.toggle("opacity-100");
-        usual.classList.toggle("top-0");
-        usual.classList.toggle("inset-y-32");
+document.getElementById("header-scrolldown").addEventListener("click", () => {
+  document.querySelector("#cardparent").scrollIntoView({ behavior: "smooth" });
+  return false;
+});
 
-        context.classList.toggle("opacity-100");
-        context.classList.toggle("opacity-0");
-        context.classList.toggle("mt-56");
-        context.classList.toggle("mt-10");
-      });
-  }
+let els = document.querySelectorAll(".icon");
+els.forEach((el) => {
+  //el =els[el];
+  console.log(el);
+  el.addEventListener("click", () => {
+    document
+      .querySelector("#" + el.getAttribute("data-href"))
+      .scrollIntoView({ behavior: "smooth" });
+    return false;
+  });
+});
+const setListener = (t) => {
+  document.getElementById(t + "-extend").addEventListener("click", (e) => {
+    context = document.getElementById(t + "-context");
+    usual = document.getElementById(t + "-usual");
+    console.log(usual);
+    usual.classList.toggle("opacity-0");
+    usual.classList.toggle("opacity-100");
+    usual.classList.toggle("top-0");
+    usual.classList.toggle("inset-y-32");
+
+    context.classList.toggle("opacity-100");
+    context.classList.toggle("opacity-0");
+    context.classList.toggle("mt-56");
+    context.classList.toggle("mt-10");
+  });
+};
 let context, usual;
 const toggles = ["mirror", "f"];
 for (t in toggles) {
   t = toggles[t];
-setListener(t)
+  setListener(t);
 }
